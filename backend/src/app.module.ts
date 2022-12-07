96050f4781bd988712ca34d867a8314ec8b265a3
brokkko <nsstrannikova@mail.ru>
2022-12-08 01:13:06 +0300

    fixed broker data

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,12 +6,15 @@ import { BrokersService } from './brokers/brokers.service';
 import {RedisProviderService} from "./providers/redis/redis-provider.service";
 import {RedisProviderModule} from "./providers/redis/redis-provider.module";
 import {BrokersRepository} from "./brokers/brokers.repository";
+import {StocksController} from "./stocks/stocks.controller";
+import {StocksService} from "./stocks/stocks.service";
+import {StocksRepository} from "./stocks/stocks.repository";
 
 @Module({
   imports: [
     RedisProviderModule,
   ],
-  controllers: [AppController, BrokersController],
-  providers: [AppService, BrokersService, RedisProviderService, BrokersRepository],
+  controllers: [AppController, BrokersController, StocksController],
+  providers: [AppService, BrokersService, RedisProviderService, BrokersRepository, StocksService, StocksRepository],
 })
 export class AppModule {}
